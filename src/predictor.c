891398d38bda316f7284da55797f0609796a1128
h9jiang <h9jiang@eng.ucsd.edu>
2020-05-24 21:30:41 -0700

    first

--- src/predictor.c
+++ src/predictor.c
@@ -6,6 +6,8 @@
 //  described in the README                               //
 //========================================================//
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h> 
 #include "predictor.h"
 
 //
@@ -36,7 +38,16 @@ int verbose;
 //
 //TODO: Add your own Branch Predictor data structures here
 //
-
+uint32_t gshare_ghistory;
+uint32_t gshare_ghistory_limit;
+struct Node {
+  uint32_t index;
+  uint8_t  state;
+  struct Node *next;
+};
+struct Node dummy;
+struct Node* dummyPointer = &dummy;
+struct Node* tail = &dummy;
 
 //------------------------------------//
 //        Predictor Functions         //
@@ -44,18 +55,55 @@ int verbose;
 
 // Initialize the predictor
 //
+
+void 
+gshare_init_predictor() {
+  gshare_ghistory = 0;
+  gshare_ghistory_limit = pow(2 , ghistoryBits);
+}
+
 void
 init_predictor()
 {
   //
   //TODO: Initialize Branch Predictor Data Structures
   //
+  gshare_init_predictor();
 }
+
+
 
 // Make a prediction for conditional branch instruction at PC 'pc'
 // Returning TAKEN indicates a prediction of taken; returning NOTTAKEN
 // indicates a prediction of not taken
 //
+uint8_t gshare_get_prediction(uint32_t index) {
+  struct Node* walk = dummyPointer->next;
+  u_int8_t ret = 0;
+  while (walk != NULL) {
+    // printf("    walk->index = %d , walk->state = %d \n" , walk->index , walk->state);
+    if (walk->index == index) {
+      // printf("    find record %d\n" , walk->state);
+      return walk->state;}
+    walk = walk->next;
+  }
+  // printf("    No record \n");
+  return ret;
+}
+
+uint8_t
+gshare_make_prediction(uint32_t pc) {
+  // gshare_ghistory is $ghistoryBits$ bits
+  uint32_t index = (pc%gshare_ghistory_limit) ^ gshare_ghistory;
+  // printf("****function make prediction pc = %d , index = %d \n" , pc , index);
+  uint8_t predict = gshare_get_prediction(index);
+  // printf("  return predict = %d \n" , predict);
+  if (predict >= 2) predict = 1;
+  else predict = 0;
+  return predict;
+  // return NOTTAKEN;
+}
+
 uint8_t
 make_prediction(uint32_t pc)
 {
@@ -68,12 +116,12 @@ make_prediction(uint32_t pc)
     case STATIC:
       return TAKEN;
     case GSHARE:
+      return gshare_make_prediction(pc);
     case TOURNAMENT:
     case CUSTOM:
     default:
       break;
   }
-
   // If there is not a compatable bpType then return NOTTAKEN
   return NOTTAKEN;
 }
@@ -82,10 +130,52 @@ make_prediction(uint32_t pc)
 // outcome 'outcome' (true indicates that the branch was taken, false
 // indicates that the branch was not taken)
 //
+void gshare_train_predictor(uint32_t pc , uint8_t outcome) {
+  uint32_t index = (pc % gshare_ghistory_limit) ^ gshare_ghistory;
+  // printf ("****function train predictor with pc = %u , outcome = %u , gshare = %u , index = %u\n" , pc , outcome , gshare_ghistory ,  index);
+  struct Node* walk = dummyPointer->next;
+
+  uint8_t inside = 0;
+  while (walk != NULL) {
+    // printf("    walk->index = %d , walk->state = %d \n" , walk->index , walk->state);
+    // update table
+    if (walk->index == index) {
+      inside = 1;
+      uint8_t res = walk->state;
+      // printf ("       update walk->state %u outcome = %u " , res , outcome);
+      if (outcome == 1) {
+        if (res != 3) res += 1;
+      } else {
+        if (res != 0) res -= 1;
+      }
+      // printf (" %d \n" , res);
+      walk->state = res;
+      break;
+    }
+    walk = walk->next;
+  }
+  
+  if (inside == 0) {
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node)); 
+    new_node->index = index;
+    new_node->state = 1 - (outcome == 0 ? 1 : -1);
+    new_node->next = NULL;
+    tail->next = new_node;
+    tail = new_node;
+    // printf("  new node ~~ with index = %d , state = %d \n" , new_node->index , new_node->state);
+  } 
+
+  gshare_ghistory = gshare_ghistory << 1;
+  gshare_ghistory += outcome == 1 ? 1 : 0;
+  gshare_ghistory %= gshare_ghistory_limit;
+}
+
 void
 train_predictor(uint32_t pc, uint8_t outcome)
 {
   //
   //TODO: Implement Predictor training
   //
+  gshare_train_predictor(pc , outcome);
 }
+
